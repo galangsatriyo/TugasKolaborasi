57e1ab2e0e4a190fc6552ca4e61924cfefdc4df5
Akbar Bagus Wicaksana <110794387+Akbarbagusw@users.noreply.github.com>
2025-10-02 16:02:13 +0700

    Akbar Modify

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main (String [] args){
         System.out.println("Hal ini kode baris pertama");
+        System.out.println("ini baris baru hasil kolaborasi");
     }
 }
