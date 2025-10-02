563351063c3e7b3fc020466e8c97cc35d99b10bd
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-02 16:10:40 +0700

    Jopan Modify

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main (String [] args){
         System.out.println("Hal ini kode baris pertama");
         System.out.println("ini baris baru hasil kolaborasi");
+        System.out.println("hai aku lek jacob");
     }
 }
